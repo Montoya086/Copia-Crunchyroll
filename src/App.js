36aebf7a6d98f122fe95f8763d70c9090e3a10ad
Montoya086 <mon21552@uvg.edu.gt>
2023-04-27 22:18:48 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react"
+import logo_blanco from "./images/logo_blanco.png"
+import logo_normal from "./images/logo_normal.png"
 function App() {
+  const[logoHover, setLogoHover]=useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <nav className="header">
+        <div className="header-logo" onMouseEnter={()=>setLogoHover(true)} onMouseLeave={()=>setLogoHover(false)}>
+          <img src={logo_normal} className={`logo ${logoHover ? "" : "logo-top"}`}/>
+          <img src={logo_blanco} className={`logo ${logoHover ? "logo-top" : ""}`}/>
+        </div>
+        <div className="header-menu">
+          <div className="explorar-button header-button">
+            <div className="header-menu-text">
+              <p>Explorar</p>
+              <div className="dropdown-arrow"/>
+            </div>
+          </div>
+          <div className="manga-button header-button">
+            <div className="header-menu-text">
+              <p>Manga</p>
+            </div>
+          </div>
+          <div className="juegos-button header-button">
+            <div className="header-menu-text">
+              <p>Juegos</p>
+            </div>
+          </div>
+          <div className="noticias-button header-button">
+            <div className="header-menu-text">
+              <p>Noticias</p>
+              <div className="dropdown-arrow"/>
+            </div>
+          </div>
+        </div>
+        <div className="header-options"></div>
+      </nav>
     </div>
   );
 }

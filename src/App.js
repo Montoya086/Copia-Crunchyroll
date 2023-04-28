e85ffdabd9504635697bef1b17c682060fd051fb
Montoya086 <mon21552@uvg.edu.gt>
2023-04-28 11:55:31 -0600

    explorar dropdown menu

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import lists from "./images/list.png"
 import user from "./images/user.png"
 function App() {
   const[logoHover, setLogoHover]=useState(false)
+  const[explorarMenu, setExplorarMenu]=useState(false)
   return (
-    <div className="body">
+    <div className="content">
       <nav className="header">
         <div className="header-logo" onMouseEnter={()=>setLogoHover(true)} onMouseLeave={()=>setLogoHover(false)}>
           <img alt="" src={logo_normal} className={`logo ${logoHover ? "" : "logo-top"}`}/>
@@ -16,9 +17,38 @@ function App() {
         </div>
         <div className="header-menu">
           <div className="explorar-button header-button">
-            <div className="header-menu-text">
+            <div className="header-menu-text" onClick={()=>setExplorarMenu(!explorarMenu)}>
               <p>Explorar</p>
               <div className="dropdown-arrow"/>
+            </div>
+            <div className={`explorar-menu-dropdown ${explorarMenu ? "state-open" : ""}`}>
+              <div className="content">
+                <div className="menu-selection">
+                  <ul className="menu-list">
+                    <li className="list-item">
+                      <p>Popular</p>
+                    </li>
+                    <li className="list-item">
+                      <p>Novedades</p>
+                    </li>
+                    <li className="list-item">
+                      <p>Alfabético</p>
+                    </li>
+                    <li className="list-item">
+                      <p>Temporada de Simulcasts</p>
+                    </li>
+                    <li className="list-item">
+                      <p>Calendario de lanzamientos</p>
+                    </li>
+                    <li className="list-item">
+                      <p>Videos musicales y conciertos</p>
+                    </li>
+                  </ul>
+                </div>
+                <div className="genero-selection">
+
+                </div>
+              </div>
             </div>
           </div>
           <div className="manga-button header-button">
@@ -60,6 +90,9 @@ function App() {
           </div>
         </div>
       </nav>
+      <div className="app-body">
+
+      </div>
     </div>
   );
 }

18f74d2bb23c858a0d77c13ceea62284045ad2e1
Montoya086 <mon21552@uvg.edu.gt>
2023-04-28 18:54:00 -0600

    explorar and noticias

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import user from "./images/user.png"
 function App() {
   const[logoHover, setLogoHover]=useState(false)
   const[explorarMenu, setExplorarMenu]=useState(false)
+  const[noticiasMenu, setNoticiasMenu]=useState(false)
   return (
     <div className="content">
       <nav className="header">
@@ -16,8 +17,8 @@ function App() {
           <img alt="" src={logo_blanco} className={`logo ${logoHover ? "logo-top" : ""}`}/>
         </div>
         <div className="header-menu">
-          <div className="explorar-button header-button">
-            <div className="header-menu-text" onClick={()=>setExplorarMenu(!explorarMenu)}>
+          <div className={`explorar-button header-button ${explorarMenu ? "menu-opened" : ""}`}>
+            <div className="header-menu-text" onClick={()=>{setExplorarMenu(!explorarMenu); if(noticiasMenu){setNoticiasMenu(false)} }}>
               <p>Explorar</p>
               <div className="dropdown-arrow"/>
             </div>
@@ -46,7 +47,24 @@ function App() {
                   </ul>
                 </div>
                 <div className="genero-selection">
-
+                  <p className="genero-title">GÉNEROS</p>
+                  <div className="genero-elements">
+                    <li><p>Acción</p></li>
+                    <li><p>Aventura</p></li>
+                    <li><p>Comedia</p></li>
+                    <li><p>Drama</p></li>
+                    <li><p>Fantasia</p></li>
+                    <li><p>Musical</p></li>
+                    <li><p>Romance</p></li>
+                    <li><p>Ciencia Ficción</p></li>
+                    <li><p>Seinen</p></li>
+                    <li><p>Shoujo</p></li>
+                    <li><p>Shounen</p></li>
+                    <li><p>Recuentos de la Vida</p></li>
+                    <li><p>Deportes</p></li>
+                    <li><p>Sobrenatural</p></li>
+                    <li><p>Thriller</p></li>
+                  </div>
                 </div>
               </div>
             </div>
@@ -61,10 +79,20 @@ function App() {
               <p>Juegos</p>
             </div>
           </div>
-          <div className="noticias-button header-button">
+          <div className={`explorar-button header-button ${noticiasMenu ? "menu-opened" : ""}`} onClick={()=>{setNoticiasMenu(!noticiasMenu); if(explorarMenu){setExplorarMenu(false)}}}>
             <div className="header-menu-text">
               <p>Noticias</p>
               <div className="dropdown-arrow"/>
+            </div>
+            <div className={`noticias-menu-dropdown ${noticiasMenu ? "state-open" : ""}`}>
+              <div className="noticias-list">
+                <ul>
+                  <li><p>Todas las noticias</p></li>
+                  <li><p>Anime Awards</p></li>
+                  <li><p>Crunchyroll Expo</p></li>
+                  <li><p>Anime Movie Night</p></li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
